90f16db007e5fc61345e7be862ffef35cb1dc75e
Deepan Reguraman <Deepan.Reguraman@centurylink.com>
2019-08-07 14:24:28 +0530

    more on streams

--- src/com/java/lambda/streams/StreamExample1.java
+++ src/com/java/lambda/streams/StreamExample1.java
@@ -2,6 +2,7 @@ package com.java.lambda.streams;
 
 import java.util.Arrays;
 import java.util.List;
+import static java.util.stream.Collectors.toList;
 
 import com.java.lambda.exercise1.Person;
 
@@ -13,8 +14,20 @@ public class StreamExample1 {
 
 		personList.stream().filter(p -> p.getFirstName().startsWith("H"))
 				.forEach(p -> System.out.println(p.getFirstName()));
-		
+
 		System.out.println(personList.parallelStream().filter(p -> p.getLastName().startsWith("M")).count());
+
+		List<String> nameList = personList.stream().filter(person -> person.getAge() > 39)
+				.map(person -> person.getFirstName()).map(fName -> fName.toUpperCase()).collect(toList());
+		System.out.println("Lambda Streams : " + nameList);
+
+		/**
+		 * Here Person class has getFirstName method and String Class has toUpperCase
+		 * method
+		 */
+		nameList = personList.stream().filter(person -> person.getAge() > 39).map(Person::getFirstName)
+				.map(String::toUpperCase).collect(toList());
+		System.out.println("Lambda Streams Method References : " + nameList);
 	}
 
 }
