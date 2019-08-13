a194895668408b2d4bb750252b2eca49fb836f21
Deepan Reguraman <Deepan.Reguraman@centurylink.com>
2019-08-13 18:24:32 +0530

    more examples

--- src/com/java/lambda/streams/StreamExample1.java
+++ src/com/java/lambda/streams/StreamExample1.java
@@ -1,9 +1,14 @@
 package com.java.lambda.streams;
 
 import java.util.Arrays;
-import java.util.List;
-import static java.util.stream.Collectors.toList;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 import com.java.lambda.exercise1.Gender;
 import com.java.lambda.exercise1.Person;
 
@@ -12,6 +17,7 @@ public class StreamExample1 {
 	public static void main(String[] args) {
 		List<Person> personList = Arrays.asList(new Person("Hero", "Maestro", Gender.MALE, 65),
 				new Person("Honda", "Unicorn", Gender.MALE, 39), new Person("Benz", "Mercedes", Gender.FEMALE, 37),
+				new Person("Hero", "Master", Gender.MALE, 2), new Person("Benz", "Merry", Gender.MALE, 7),
 				new Person("ThunderBird", "Enfield", Gender.MALE, 70));
 
 		personList.stream().filter(p -> p.getFirstName().startsWith("H"))
@@ -43,6 +49,49 @@ public class StreamExample1 {
 				.max((person1, person2) -> person1.getAge().compareTo(person2.getAge()))
 				.map(person -> person.getFirstName().toUpperCase()));
 
+		List<String> majorList = personList.stream().filter(person -> person.getAge() > 17).map(Person::getFirstName)
+				.collect(toList());
+		System.out.println("majorList : " + majorList);
+
+		System.out.println("Male Gender List : " + personList.stream()
+				.filter(person -> person.getGender() == Gender.MALE).map(Person::getFirstName).collect(toList()));
+
+		/**
+		 * Set will remove duplicates, so Hero occurs twice but once only printed.
+		 */
+		System.out.println("Male Gender Set : " + personList.stream()
+				.filter(person -> person.getGender() == Gender.MALE).map(Person::getFirstName).collect(toSet()));
+
+		Map<String, List<Person>> myMap = personList.stream().filter(person -> person.getGender() == Gender.MALE)
+				.collect(Collectors.groupingBy(Person::getFirstName));
+		System.out.println("Male Gender Map : " + myMap);
+
+		/**
+		 * This will no work
+		 * 
+		 * personList.stream().filter(person -> person.getGender() ==
+		 * Gender.MALE).map(Person::getFirstName).collect(Collectors.groupingBy(Person::getFirstName));
+		 * 
+		 */
+
+		// personList.stream().filter(person -> person.getAge() >
+		// 17).collect(Collectors.)
+
+		System.out.println("Matching Name : "
+				+ personList.stream().filter(person -> person.getFirstName().length() == 4).findFirst());
+		
+		System.out.println("Matching Name Another Way : "
+				+ personList.stream().filter(StreamExample1::isFourLettersFirstName).findFirst());
+		
+		System.out.print("Print 10 Even Numbers : ");
+		Stream.iterate(0, e -> e + 1).filter(e -> e % 2 == 0).limit(10).forEach(System.out::println);
+		
+		System.out.print("Print Even Numbers Starting from 4 (seed first param in iterate) : ");
+		Stream.iterate(4, e -> e+1).filter(e -> e%2 ==0).limit(10).forEach(System.out::println);
+	}
+
+	private static boolean isFourLettersFirstName(Person person) {
+		return person.getFirstName().length() == 4;
 	}
 
 }
